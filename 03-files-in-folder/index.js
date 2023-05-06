87155ce72d00bc49625a50371484e3370609bfcd
Uladimir Hrydzin <hrydzin@gmail.com>
2023-05-06 13:11:16 +0200

    feat: add solution of task3

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,18 @@
+
+const fs = require('fs')
+const path = require('path');
+
+fs.readdir(path.join(__dirname, '/secret-folder/'), {withFileTypes: true}, (err, files) => {
+    if (err) throw err;
+    for (let file of files) {
+            let fileArg  = path.join(__dirname, '/secret-folder/', file.name) ;
+            fs.stat(fileArg, (err, stats) => {
+                if (err) throw err;
+                if (file.isFile()) {
+                    let nameFile = (file.name).replace(path.extname(file.name), '') ;
+                    let extnameFile = path.extname(file.name).slice(1)
+                    console.log (`${nameFile} - ${extnameFile} - ${stats.size}b`);
+                }
+            })
+    }
+})
